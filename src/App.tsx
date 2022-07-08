f349a2decd998029bd66b3e9f962b688d4e1eac9
Yannick Ferire <yannick@bureau347.com>
2022-07-08 04:56:44 +0200

    vite - react - ts - tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-amber-300">
+      TFT Sandbox
+      </h1>
+    </div>
+  )
+}
+
+export default App
