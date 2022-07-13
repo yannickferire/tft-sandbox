8cd84ab6878278ef3c9c6d0597e249112fe6704a
Yannick Ferire <yannick@bureau347.com>
2022-07-13 15:38:22 +0200

    Rollling chances with real data from json

--- src/components/hud/rollingChances/index.tsx
+++ src/components/hud/rollingChances/index.tsx
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './styles.css';
 
+import rollingData from '../../../data/rolling.json';
+
+// const lvl = "lvl7";
+const lvlData = rollingData.lvl2;
+
 class RollingChances extends Component {
   render() {
     return (
       <ul id="rolling-chances" className="flex absolute text-xs">
-        <li id="r-c-1-cost" className="flex-1 pl-4 relative mr-2"><span className="cost-shape">1 cost</span> 20%</li>
-        <li id="r-c-2-cost" className="flex-1 pl-4 relative mr-2"><span className="cost-shape ">2 cost</span> 20%</li>
-        <li id="r-c-3-cost" className="flex-1 pl-4 relative mr-2"><span className="cost-shape">3 cost</span> 20%</li>
-        <li id="r-c-4-cost" className="flex-1 pl-4 relative mr-2"><span className="cost-shape">4 cost</span> 20%</li>
-        <li id="r-c-5-cost" className="flex-1 pl-4 relative mr-2"><span className="cost-shape">5 cost</span> 20%</li>
+        {Object.entries(lvlData).map(rollChance => (
+          <li id={"r-c-"+rollChance[0]} className="flex-1 pl-4 relative mr-2"><span className="cost-shape">{rollChance[0]}</span> {rollChance[1]}%</li>
+        ))}
       </ul>
     );
   }
