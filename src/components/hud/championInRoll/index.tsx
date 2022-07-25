17de19f33770b904bab431279a50d99a637caead
Yannick Ferire <yannick@bureau347.com>
2022-07-26 01:43:13 +0200

    ChampionInRoll : Add spend function on click

--- src/components/hud/championInRoll/index.tsx
+++ src/components/hud/championInRoll/index.tsx
@@ -1,25 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { RootState, Dispatch } from '../../../store/store';
+import { connect } from 'react-redux';
 import './styles.css';
 
 import coinIcon from '../../../assets/images/icons/coin.png';
 
-class ChampionInRoll extends Component {
+const championName = "Yasuo"
+const championCost = 5
+
+class ChampionInRoll extends React.PureComponent<Props> {
   render() {
+    const { goldsState } = this.props
     return (
-      <li className="overflow-hidden five-cost">
-        <img className="z-0 absolute max-w-none" src="https://raw.communitydragon.org/pbe/game/assets/ux/tft/championsplashes/tft7_yasuo.tft_set7.png" alt="Yasuo - 5 cost" />
+      <li onClick={goldsState >= championCost ? this.props.spend : undefined} className={"overflow-hidden cost-"+championCost}>
+        <img className="z-0 absolute max-w-none" src="https://raw.communitydragon.org/pbe/game/assets/ux/tft/championsplashes/tft7_yasuo.tft_set7.png" alt={championName + championCost +" cost"} />
         <ul className="z-20 absolute bottom-9 left-3 text-sm leading-relaxed">
           <li>Mirage</li>
           <li>Warrior</li>
           <li>Dragonmancer</li>
         </ul>
         <div className="z-20 flex w-full absolute bottom-1 px-3 text-shadow font-semibold">
-          <span className="flex-1">Yasuo</span>
-          <span><img className="inline-block -ml-0.5 -mt-0.5" src={coinIcon} alt="Coins" width="10" /> 5</span>
+          <span className="flex-1">{championName}</span>
+          <span><img className="inline-block -ml-0.5 -mt-0.5" src={coinIcon} alt="Coins" width="10" /> {championCost}</span>
         </div>
       </li>
     );
   }
 }
 
-export default ChampionInRoll;
+const mapState = (state: RootState) => ({
+	goldsState: state.golds,
+})
+
+const mapDispatch = (dispatch: Dispatch) => ({
+	spend: () => dispatch.golds.spend(championCost),
+})
+
+type StateProps = ReturnType<typeof mapState>
+type DispatchProps = ReturnType<typeof mapDispatch>
+type Props = StateProps & DispatchProps
+
+export default connect(mapState, mapDispatch)(ChampionInRoll);
